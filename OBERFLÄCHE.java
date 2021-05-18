5c72543b5d41a6437029da5e02a7b88341dc19ae
Le-Dek <dekinglea@gmail.com>
2021-05-18 09:20:06 +0200

    UI QUIZ in OBERFLÄCHE eingefügt

--- OBERFLÄCHE.java
+++ OBERFLÄCHE.java
@@ -1,31 +1,56 @@
 
-/**
- * Beschreiben Sie hier die Klasse OBERFLÄCHE.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
- */
+import java.awt.*;
+import java.awt.event.*;
+
 class OBERFLÄCHE
 {
-    {FRAGE fragefeld;
-    JOKER jokerfeld;
+    Frame fenster;
+    Label fragetext;
+    Button antwortA;
+    Button antwortB;
+    Button antwortC;
+    Button antwortd;
+
     /**
-     * 
-     *PUNKTESTAND punktefeld;
-     *ANTWORT möglichkeiten;
-    int x;
-    int y;
-    OBERFLÄCHE()
+     * Constructor for objects of class OBERFLAECHE
+     */
+    public OBERFLÄCHE()
     {
-       fragefeld = new FRAGE();
-       fragefeld.FarbeSetzen();
-       fragefeld.PositionSetzen(x, y);
-       jokerfeld = new JOKER();
-       jokerfeld.FarbeSetzen();
-       jokerfeld.PositionSetzen(x, y);
-       punktefeld = new PUNKTESTAND();
-       punktefeld.FarbeSetzen();
-       punktefeld.PositionSetzen(x, y);     
-       */ 
-    }   
+        fenster = new Frame("UI");
+        fenster.setSize(600,400);
+        fenster.setLocation(0,30);
+        fenster.setVisible(true);
+        fenster.setLayout(null);
+        
+        fragetext = new Label();
+        fragetext.setText("Hier steht später die Frage und ggf. die Antworten.");
+        fragetext.setVisible(true);
+        fragetext.setLocation(200,50);
+        fragetext.setSize(400,50);
+        fenster.add(fragetext);
+        
+        antwortA = new Button();
+        antwortA.setLocation(10,250);
+        antwortA.setLabel("A");
+        antwortA.setVisible(true);
+        antwortA.setSize(100,30);
+        antwortA.setEnabled(true);
+        antwortA.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                //Hier wird Frage A ausgewählt
+            }
+        });
+        fenster.add(antwortA);
+        
+        fenster.addWindowListener(new WindowAdapter()
+        {
+            public void windowClosing(WindowEvent e)
+            {
+                System.exit(1);
+            }
+        });
+    }
 }
+
